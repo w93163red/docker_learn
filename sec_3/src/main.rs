d7e75e592eed186a09227084a67ed22decb9fb91
LinG <w93163red@gmail.com>
2022-08-21 19:57:06 -0400

    test sec_3

--- sec_3/src/main.rs
+++ sec_3/src/main.rs
@@ -1,3 +1,16 @@
+use unshare::Namespace;
+use unshare::Stdio;
+
 fn main() {
-    println!("Hello, world!");
+    let mut namespaces = Vec::<Namespace>::new();
+    namespaces.push(Namespace::Pid);
+    namespaces.push(Namespace::Mount);
+    // let mut cmd = std::env::args().nth(1);
+    let mut command = unshare::Command::new("ls")
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        // .unshare(&namespaces)
+        .spawn()
+        .expect("Command cannot spawn");
+    let _eproc = command.wait().expect("failed to wait for the process");
 }
